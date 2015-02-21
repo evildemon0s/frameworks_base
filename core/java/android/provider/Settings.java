ad8d2ca7dedcf9885403a885bfc82696db6c6518
rogersb11 <brettrogers11@gmail.com>
2015-02-21 01:10:08 -0500

    AdBlocker 2/3
    Change-Id: I4bf5f55021cf234e47cf344d0183b24b1550ca4e
    credit: tsubus
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3736,6 +3736,13 @@ public final class Settings {
          * @hide
          */
         public static final String RECENTS_SEARCH_BAR = "recents_search_bar";
+        
+        /**
+         * Whether to mute annoying notifications
+		 * Disable ads (HFM)
+		 * @hide
+		 */
+		public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
 
 
         /**
