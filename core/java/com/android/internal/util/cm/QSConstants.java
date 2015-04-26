41ee6c7f737d7b551a76b76c4caba4fe23ca88c4
Noeljunior <liamgliam@gmail.com>
2015-04-26 11:46:43 +0100

    QS: Add Ambient Display Tile (1/2)
    Change-Id: I31ffd7d83a65db294759374900da0fca1c760549

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -50,6 +50,7 @@ public class QSConstants {
     public static final String TILE_MUSIC = "music";
     public static final String TILE_CANDY = "candy_settings";
     public static final String TILE_HEADS_UP = "heads_up";
+    public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -90,5 +91,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_MUSIC);
         TILES_AVAILABLE.add(TILE_CANDY);
         TILES_AVAILABLE.add(TILE_HEADS_UP);
+        TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
     }
 }
