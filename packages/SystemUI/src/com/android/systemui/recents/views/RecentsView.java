49ff8bb34d8c59756da82739a44f9e04ab09e7d2
Roman Birg <roman@cyngn.com>
2015-05-13 10:39:07 -0700

    SystemUI: add RecentsView EventLogs
    Change-Id: Iec202e9f764fce85eaa4ddff7c8f311146b00229
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -29,6 +29,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.Gravity;
+import android.util.EventLog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowInsets;
@@ -42,6 +43,7 @@ import com.android.systemui.recents.model.Task;
 import com.android.systemui.recents.model.TaskStack;
 
 import com.android.systemui.R;
+import com.android.systemui.EventLogTags;
 
 import java.util.ArrayList;
 
@@ -232,6 +234,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
             }
         }
         ctx.postAnimationTrigger.decrement();
+
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 1 /* opened */);
     }
 
     /** Requests all task stacks to start their exit-recents animation */
@@ -414,6 +418,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 startHideClearRecentsButtonAnimation();
 
                 dismissAllTasksAnimated();
+
+                EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 4 /* closed all tasks */);
             }
         });
     }
@@ -632,6 +638,8 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 launchRunnable.run();
             }
         }
+
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 3 /* chose task */);
     }
 
     @Override
@@ -674,6 +682,7 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
                 stackView.onRecentsHidden();
             }
         }
+        EventLog.writeEvent(EventLogTags.SYSUI_RECENTS_EVENT, 2 /* closed */);
     }
 
     @Override
